d9f26921ef6faaa1e8978960913a12af51c13a2f
Emil Engberg <emil.engberg@outlook.com>
2021-10-25 16:23:07 +0200

    Removed temp file.

--- init.el
+++ init.el
@@ -120,14 +120,3 @@
     (local-set-key (kbd "C-c <down>")  'hs-show-all)
     (hs-minor-mode t)
     (hs-hide-all)))
-
-(use-package general
-  :config
-  (general-create-definer rune/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-
-  (rune/leader-keys
-    "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")))
