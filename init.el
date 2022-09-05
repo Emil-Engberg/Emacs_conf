1574795de6ba66f420b43031b8d0222ad1ff93c8
Emil Engberg <emil.engberg@outlook.com>
2022-09-05 12:30:42 +0200

    Added flymake to c and prolog.

--- init.el
+++ init.el
@@ -130,8 +130,10 @@
     (hs-minor-mode t)
     (hs-hide-all)
     (c-set-style "gnu")
+    (c-set-offset 'case-label '+)
     (setq c-basic-offset 4)
-    (setq tab-width 4)))
+    (setq tab-width 4)
+    (flymake-mode 1)))
 (use-package magit)
 
 (dolist (hook '(text-mode-hook))
@@ -159,3 +161,30 @@ apps are not started from a shell."
     (setq exec-path (split-string path-from-shell path-separator))))
 
 (set-exec-path-from-shell-PATH)
+(autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
+(autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
+(autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
+(setq prolog-system 'swi)
+(setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
+                              ("\\.m$" . mercury-mode))
+                              auto-mode-alist))
+
+(add-hook 'prolog-mode-hook
+          (lambda ()
+            (require 'flymake)
+            (make-local-variable 'flymake-allowed-file-name-masks)
+            (make-local-variable 'flymake-err-line-patterns)
+            (setq flymake-err-line-patterns
+                  '(("ERROR: (?\\(.*?\\):\\([0-9]+\\)" 1 2)
+                    ("Warning: (\\(.*\\):\\([0-9]+\\)" 1 2)))
+            (setq flymake-allowed-file-name-masks
+                  '(("\\.pl\\'" flymake-prolog-init)))
+            (flymake-mode 1)))
+
+(defun flymake-prolog-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+		       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "swipl" (list "-q" "-g" "use_module(library(diagnostics))" "-t" "halt" "--" "test.pl"))))
